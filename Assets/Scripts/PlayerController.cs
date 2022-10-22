17873280348eac92d9d7d27f0bbd948c13652a59
MattSantana <msantana.oliveira22@gmail.com>
2022-10-22 19:07:21 -0300

    reload e bug da camera saiu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -147,7 +148,7 @@ public class PlayerController : MonoBehaviour
         life -= damage;
         if( life <= 0 )
         {
-           Destroy(gameObject);
+           SceneManager.LoadScene(2);
         }
     }
 
