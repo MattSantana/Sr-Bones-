98a36a4705f49783fd264bfa76fde5e892e7764a
Ana Júlia <art.opalablue@gmail.com>
2022-10-22 04:11:01 -0300

    Inicio transição

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,10 +6,29 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
     public string StartLevel;
+    public GameObject fader;
+    public GameObject faderIn;
+    public float transitionTime;
 
-    public void LoadLevel()
+    void Start()
     {
-        SceneManager.LoadScene(StartLevel);
+        faderIn = GameObject.FindGameObjectWithTag("InTransition");
+        faderIn.transform.position = new Vector3(-50, 0, 0);
+        fader = GameObject.FindGameObjectWithTag("OutTransition");
+        fader.LeanMoveX(-50, transitionTime);
+    }
+
+    IEnumerator PlayTransition(string sceneName)
+    {
+        faderIn.LeanMoveX(10 , transitionTime);
+        yield return new WaitForSeconds(transitionTime + 1);
+        SceneManager.LoadScene(sceneName);
         Debug.Log("Começou");
+
+    }
+
+    public void GotoScene(string SceneName)
+    {
+        StartCoroutine(PlayTransition(SceneName));
     }
 }
